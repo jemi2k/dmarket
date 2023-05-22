e7730d7598664dfa33062638b435d0df042794b5
Ermias <jermijwll@gmail.com>
2023-05-22 14:52:24 -0700

    ipfs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["assets.vercel.com", "infura-ipfs.io", "ipfs.infura.io"],
+    domains: ["assets.vercel.com", "infura-ipfs.io", "ipfs.infura.io", "dmarket.infura-ipfs.io"],
     //domains: ["assets.vercel.com", "infura-ipfs.io", ""],
     formats: ["image/avif", "image/webp"],
   },
