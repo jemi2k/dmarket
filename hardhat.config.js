3614826777ad2725d7a0930ddb54c62a09a045df
Ermias <jermijwll@gmail.com>
2023-05-22 14:38:07 -0700

    hard

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,7 +13,7 @@ module.exports = {
       chainId: 1337,
     },
     mumbai: {
-    //  url: `https://polygon-mumbai.infura.io/v3/${PROJECT_ID}`,
+    
       url: `https://polygon-mumbai.g.alchemy.com/v2/${PROJECT_ID}`,
       accounts: [privateKey],
     },
