497318a0ca04064109f89aa0aa3a41968d26024a
Ermias <jermijwll@gmail.com>
2023-05-24 04:52:52 -0700

    update ipfs key

--- utils/constants.js
+++ utils/constants.js
@@ -12,14 +12,14 @@ const TEXTAREA_MAX_LENGTH = 255;
 const CRYPTO_CURRENCY = "ether";
 
 //const INFURA_URL = "ipfs.infura.io";
-const INFURA_URL = "dmarket.infura-ipfs.io";
+const INFURA_URL = "dmarket1.infura-ipfs.io";
 //const INFURA_URL = "infura-ipfs.io";
 
 
 
 const PROJECT_ID = "zErRQKT2IPg__bQGcm2TbNW86LFJHhym";
 
-const IPFS_PROJECT_ID = "2Q2C5vJtC5wYD9up5PBBKmDJVsk";
+const IPFS_PROJECT_ID = "2QEqDQlCavMFLAysVS9hfjKhOaB";
 
 const ACTION_TYPES = {
   LIST_ITEM: "list_item",
