2f52a148345e36ab3c1b71711bd07a1989bcfefd
Ermias <jermijwll@gmail.com>
2023-05-30 13:42:39 -0700

    imagenull

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   reactStrictMode: true,
   
   images: {
-    domains: ["assets.vercel.com", "dmarket.vercel.app", "infura-ipfs.io", "ipfs.infura.io", "dmarket.infura-ipfs.io"],
-    //domains: ["assets.vercel.com", "dmarket.infura-ipfs.io", ""],
+    //domains: ["assets.vercel.com", "dmarket.vercel.app", "infura-ipfs.io", "ipfs.infura.io", "dmarket.infura-ipfs.io"],
+    //domains: ["assets.vercel.com", "infura-ipfs.io", ""],
     formats: ["image/avif", "image/webp"],
     unoptimized: true,
     fill: true 
