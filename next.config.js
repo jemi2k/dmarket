82ff83dea7cb8c5b0f397882208dbd281ef252b9
Ermias <jermijwll@gmail.com>
2023-05-30 03:14:01 -0700

    image format

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["assets.vercel.com", "infura-ipfs.io", "ipfs.infura.io", 'dmarket.infura-ipfs.io'],
+    domains: ["assets.vercel.com", "infura-ipfs.io", "ipfs.infura.io", "dmarket.infura-ipfs.io"],
     //domains: ["assets.vercel.com", "infura-ipfs.io", ""],
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/avif", "image/webp", "image/jpeg", "image/jpg", "image/png"],
   },
     "API": {
     "HTTPHeaders": {
