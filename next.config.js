547484a6450f77976e71a3b8bac40466092bdf37
Ermias <jermijwll@gmail.com>
2023-05-30 13:49:16 -0700

    image io

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   
   images: {
     //domains: ["assets.vercel.com", "dmarket.vercel.app", "infura-ipfs.io", "ipfs.infura.io", "dmarket.infura-ipfs.io"],
-    //domains: ["assets.vercel.com", "infura-ipfs.io", ""],
+    domains: ["assets.vercel.com", "infura-ipfs.io", ""],
     formats: ["image/avif", "image/webp"],
     unoptimized: true,
     fill: true 
