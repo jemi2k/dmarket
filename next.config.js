f6a285ec76ef9459c1698fb73b8c48b891e37e96
Ermias <jermijwll@gmail.com>
2023-05-31 00:12:35 -0700

    next config dmarket1

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ module.exports = {
   reactStrictMode: true,
   
   images: {
-    domains: ["assets.vercel.com", "dmarket.vercel.app", "infura-ipfs.io", "ipfs.infura.io", "dmarket.infura-ipfs.io"],
+    domains: ["assets.vercel.com", "dmarket.vercel.app", "infura-ipfs.io", "ipfs.infura.io", "dmarket1.infura-ipfs.io"],
     //domains: ["assets.vercel.com", "infura-ipfs.io", ""],
     formats: ["image/avif", "image/webp"],
-    unoptimized: true,
-    fill: true 
+   // unoptimized: true,
+    //fill: true 
   },
     "API": {
     "HTTPHeaders": {
