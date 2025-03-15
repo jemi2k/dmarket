39c2452e8e875ff75f01af25e0745bb3db69bdac
Ermias <jermijwll@gmail.com>
2025-03-15 13:23:23 +0300

    net, and some css

--- next.config.js
+++ next.config.js
@@ -1,19 +1,24 @@
 module.exports = {
   reactStrictMode: true,
-  
+
   images: {
-    domains: ["assets.vercel.com", "dmarket.vercel.app", "infura-ipfs.io", "ipfs.infura.io", "dmarket1.infura-ipfs.io"],
-    //domains: ["assets.vercel.com", "infura-ipfs.io", ""],
+    domains: [
+      "assets.vercel.com",
+      "dmarket.vercel.app",
+      "infura-ipfs.io",
+      "ipfs.infura.io",
+      "dmarket1.infura-ipfs.io",
+      "d.atlasofshop.com",
+    ],
+    // domains: ["assets.vercel.com", "infura-ipfs.io", ""],
     formats: ["image/avif", "image/webp"],
-   //unoptimized: true,
-    //fill: true 
+    // unoptimized: true,
+    // fill: true
   },
-    "API": {
-    "HTTPHeaders": {
-      "Access-Control-Allow-Origin": [
-        "*"
-      ]
-    }
+  API: {
+    HTTPHeaders: {
+      "Access-Control-Allow-Origin": ["*"],
+    },
   },
 };
-//git config --global --unset https.proxy
+// git config --global --unset https.proxy
